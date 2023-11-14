59c113d4c854379e972da3c2ad43597fed6ad141
Aref Akminasi <137000185+Aref-Akminasi@users.noreply.github.com>
2023-11-14 13:45:52 +0100

    added directory files

--- modules.js
+++ modules.js
@@ -1,5 +1,41 @@
-const { people, ages } = require('./people');
+const fs = require('fs');
 
-console.log(people, ages);
+// reading files
+fs.readFile('./docs/blog1.txt', (err, data) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(data.toString());
+});
 
-const os = require('os');
+// writing files
+fs.writeFile('./docs/blog1.txt', 'Hello, Ninjas', () => {
+  console.log('file was written');
+});
+
+// directories
+if (!fs.existsSync('./assets')) {
+  fs.mkdir('./assets', (err) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log('folder created');
+  });
+} else {
+  fs.rmdir('./assets', (err) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log('folder deleted');
+  });
+}
+
+// deleting files
+if (fs.existsSync('./docs/deleteme.txt')) {
+  fs.unlink('./docs/deleteme.txt', (err) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log('file deleted');
+  });
+}
