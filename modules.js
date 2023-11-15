9b8000ff8993ead5ae91ac8e465686b45d2c7825
Aref Akminasi <137000185+Aref-Akminasi@users.noreply.github.com>
2023-11-15 14:32:07 +0100

    streams/buffers/pipes

--- modules.js
+++ modules.js
@@ -1,41 +1,16 @@
 const fs = require('fs');
 
-// reading files
-fs.readFile('./docs/blog1.txt', (err, data) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log(data.toString());
+const readStream = fs.createReadStream('./docs/blog3.txt', {
+  encoding: 'utf8',
 });
+const writeStream = fs.createWriteStream('./docs/blog4.txt');
 
-// writing files
-fs.writeFile('./docs/blog1.txt', 'Hello, Ninjas', () => {
-  console.log('file was written');
-});
+/*readStream.on('data', (chunk) => {
+  console.log('--- NEW CHUNK ---');
+  console.log(chunk);
+  writeStream.write('\nNEW CHUNK\n');
+  writeStream.write(chunk);
+});*/
 
-// directories
-if (!fs.existsSync('./assets')) {
-  fs.mkdir('./assets', (err) => {
-    if (err) {
-      console.log(err);
-    }
-    console.log('folder created');
-  });
-} else {
-  fs.rmdir('./assets', (err) => {
-    if (err) {
-      console.log(err);
-    }
-    console.log('folder deleted');
-  });
-}
-
-// deleting files
-if (fs.existsSync('./docs/deleteme.txt')) {
-  fs.unlink('./docs/deleteme.txt', (err) => {
-    if (err) {
-      console.log(err);
-    }
-    console.log('file deleted');
-  });
-}
+// piping
+readStream.pipe(writeStream);
