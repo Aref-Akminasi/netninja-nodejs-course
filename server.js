15ffeb3d6e9cfcfd67d49014e4c72fba1d504331
Aref Akminasi <137000185+Aref-Akminasi@users.noreply.github.com>
2023-11-17 18:30:46 +0100

    clients & servers

---
+++ server.js
@@ -0,0 +1,10 @@
+// clients & servers
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  console.log('request made');
+});
+
+server.listen(3000, 'localhost', () => {
+  console.log('listening for requests on port 3000');
+});
