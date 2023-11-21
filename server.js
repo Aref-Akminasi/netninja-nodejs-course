097580d1a58446cea60fee61fee5805af8a8d2c5
Aref Akminasi <137000185+Aref-Akminasi@users.noreply.github.com>
2023-11-21 15:36:40 +0100

    added res writing

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
 // requests & responses
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method);
+
+  // set header content type
+
+  res.setHeader('Content-Type', 'text/plain');
+
+  res.write('hello, ninjas');
+
+  res.end();
+});
+
+server.listen(3000, 'localhost', () => {
+  console.log('listening for requests on port 3000');
+});
