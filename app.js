4dc45a0e0fd61ee3a0e9385f06daececa3e7d09a
Aref Akminasi <137000185+Aref-Akminasi@users.noreply.github.com>
2023-12-02 12:25:00 +0100

    added get listeners

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+// express app
+const app = express();
+
+// listen for requests
+
+app.listen(3000);
+
+app.get('/', (req, res) => {
+  res.send('<p>home page</p>');
+});
+
+app.get('/about', (req, res) => {
+  res.send('<p>about page</p>');
+});
